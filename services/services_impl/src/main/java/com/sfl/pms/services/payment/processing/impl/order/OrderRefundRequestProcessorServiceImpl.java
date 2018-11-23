ae8a30c48b17de515d1b6be0d58a2ab67d02efbb
Hayk Mkrtchyan <hayk.mkrtchyan@sflpro.com>
2018-11-23 21:19:28 +0400

    Adding Qualifier to orderStateMutationExternalNotifierService

--- services/services_impl/src/main/java/com/sfl/pms/services/payment/processing/impl/order/OrderRefundRequestProcessorServiceImpl.java
+++ services/services_impl/src/main/java/com/sfl/pms/services/payment/processing/impl/order/OrderRefundRequestProcessorServiceImpl.java
@@ -20,6 +20,7 @@ import org.apache.commons.lang3.mutable.MutableObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -49,6 +50,7 @@ public class OrderRefundRequestProcessorServiceImpl implements OrderRefundReques
     private AdyenPaymentOperationsProcessor adyenPaymentOperationsProcessor;
 
     @Autowired
+    @Qualifier("orderStateMutationExternalHttpNotifierService")
     private OrderStateMutationExternalNotifierService orderStateMutationExternalNotifierService;
 
     /* Constructors */
